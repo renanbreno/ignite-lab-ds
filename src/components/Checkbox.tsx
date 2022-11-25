8f1dca1596d2a91a39afa02c4affcfff14b45e72
Renan Oliveira <65349948+renanbreno@users.noreply.github.com>
2022-11-25 16:14:21 -0300

    component integration and creation of the main screen

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -2,15 +2,15 @@ import { ReactNode } from 'react';
 import * as Checkbox from '@radix-ui/react-checkbox';
 import { Check } from 'phosphor-react';
 
-export interface CheckBoxProps {
-    children: ReactNode;
+export interface CheckBoxProps extends Checkbox.CheckboxProps {
+    children?: ReactNode;
     asChild?: boolean;
 }
 
-export function CheckBox({ } : CheckBoxProps) {
+export function CheckBox(props : CheckBoxProps) {
     return (
         <Checkbox.Root
-        className="w-6 h-6 p-[2px] bg-gray-800 rounded">
+        className="w-6 h-6 p-[2px] bg-gray-800 rounded" {...props}>
         <Checkbox.Indicator asChild>
             <Check weight='bold' className='h-5 w-5 text-cyan-500'/>
         </Checkbox.Indicator>
